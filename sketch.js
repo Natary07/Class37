b669180f62a889f3dd5771fd3048f93648be9e0e
Natary07 <aryanpat2007@gmail.com>
2021-02-28 12:01:33 -0600

    players

--- sketch.js
+++ sketch.js
@@ -4,9 +4,9 @@ var gameState = 0;
 var playerCount;
 
 var database;
-
+var allPlayers;
 var form, player, game;
-
+var gameState=0, distance=0;
 
 function setup(){
   canvas = createCanvas(400,400);
@@ -18,4 +18,12 @@ function setup(){
 
 
 function draw(){
+  if(playerCount===4){
+    game.update(1)
+    
+  }
+  if(gameState===1){
+    clear();
+    game.play();
+  }
 }
